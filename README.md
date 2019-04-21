150e3aa4ac4cb00d49fb072f93671cdc6bddc3f6
crazynayan <nayan@crazyideas.co.in>
2019-04-21 14:39:04 +0530

    Forms

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flask Tutorial
+This is my way of learning flask and implementing a micro blog.
+
+The tutorial link is
+https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,14 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     template = 'index.html'
+    title = 'Home'
     context = {
-        'title': 'Home',
         'user': {
             'username': 'Nayan',
         },
@@ -26,4 +27,16 @@ def index():
             },
         ],
     }
-    return render_template(template, context=context)
+    return render_template(template, title=title, context=context)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    template = 'login.html'
+    title = 'Sign In'
+    form = LoginForm()
+    if not form.validate_on_submit():
+        return render_template(template, title=title, form=form)
+    # This is a POST request and the form is validated
+    flash(f'Logged in as {form.username.data} (Remember me is: {form.remember_me.data})')
+    return redirect(url_for('index'))
