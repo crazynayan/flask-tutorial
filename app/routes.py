31ecd7927b832e2f58a6f2cce56e6df80dc0d961
crazynayan <nayan@crazyideas.co.in>
2019-04-22 14:24:25 +0530

    User Login

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,18 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import login_user, current_user, logout_user, login_required
+from werkzeug.urls import url_parse
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     template = 'index.html'
     title = 'Home'
     context = {
-        'user': {
-            'username': 'Nayan',
-        },
         'posts': [
             {
                 'author': {
@@ -32,11 +33,46 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     template = 'login.html'
     title = 'Sign In'
     form = LoginForm()
     if not form.validate_on_submit():
         return render_template(template, title=title, form=form)
     # This is a POST request and the form is validated
-    flash(f'Logged in as {form.username.data} (Remember me is: {form.remember_me.data})')
+    user = User.query.filter_by(username=form.username.data).first()
+    if user is None or not user.check_password(form.password.data):
+        flash('Invalid username or password')
+        return redirect(url_for('login'))
+    login_user(user=user, remember=form.remember_me.data)
+    next_page = request.args.get('next')
+    if not next_page or url_parse(next_page).netloc != '':
+        next_page = url_for('index')
+    return redirect(next_page)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    template = 'registration.html'
+    title = 'Registration'
+    form = RegistrationForm()
+    if not form.validate_on_submit():
+        return render_template(template, title=title, form=form)
+    # This is a POST request and the form is validated
+    # Add the user
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations! You have been registered. Please login.')
+    return redirect(url_for('login'))
+
